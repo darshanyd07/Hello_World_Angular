286a9f9f9069afa03b15ab88c85e24505c4fdc1f
champ09D <darshanmhatre709@gmail.com>
2022-11-03 22:54:47 +0530

    Added : Display BridgeLabz Using Binding File Case Add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,36 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent 
+{
   title = 'Hello_World';
+  imgUrl="../assets/bb.PNG";
+  url = "https://www.bridgelabz.com/";
+  userName:string="";
+  nameError:string="";
+
+  ngOnInit(): void
+  {
+    this.title = "Hello ,Whats Your Name ?"
+  }
+
+  onClick($event: any)
+  {
+    console.log("Save Button Is Clicked!", $event);
+    window.open(this.url, "_blank");
+  }
+
+  onInput($event:any)
+  {
+    console.log("change event occured!",$event.data);
+    const nameRegex=RegExp('[A-Z]{1}[a-z]{2}');
+    if(nameRegex.test(this.userName))
+    {
+      this.nameError="";
+      this.title=this.userName;
+      return;
+    }
+    this.nameError="Name Is Incorrect";
+  }
+
 }
